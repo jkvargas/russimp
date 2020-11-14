4bdf77e6824eb4f5b0c28ee4040eccb0b157f097
Jhonny Vargas <vargasj@ryanair.com>
2020-11-14 21:08:38 +0000

    Working on metadata.

--- russimp-core/src/metadata.rs
+++ russimp-core/src/metadata.rs
@@ -1,4 +1,5 @@
 use russimp_sys::aiMetadata;
+use std::borrow::Borrow;
 
 pub struct MetaData {
     meta_data: *mut aiMetadata
@@ -9,5 +10,12 @@ impl Into<MetaData> for *mut aiMetadata {
         MetaData {
             meta_data: self
         }
+    }
+}
+
+impl MetaData {
+    pub fn get_keys(&self) -> Vec<String> {
+        let res = unsafe { std::slice::from_raw_parts_mut((*self.meta_data).mKeys, (*self.meta_data).mNumProperties as usize) };
+        res.to_vec().into_iter().map(|x|x.into()).collect()
     }
 }
