5a9f165bbe1b0746fe0bd3232c7bd8b226e5caac
Jhonny Vargas <vargasj@ryanair.com>
2020-12-12 14:17:57 +0000

    working on publishing crate

--- russimp-sys/build.rs
+++ russimp-sys/build.rs
@@ -2,16 +2,16 @@
 
 use std::{env::var, path::PathBuf};
 
-const BINDINGS_FILE : &str = "bindings.rs";
-const WRAPPER_FILE : &str = "wrapper.h";
+const BINDINGS_FILE: &str = "bindings.rs";
+const WRAPPER_FILE: &str = "wrapper.h";
 
 fn main() {
     let assimp_path = cmake::Config::new("assimp")
         .define("CMAKE_BUILD_TYPE", "Release")
         .build();
 
-    let output_path = PathBuf::from(var("OUT_DIR").unwrap());
-    let path_bindings_buf_src = output_path.join(format!("../../../../../russimp-sys/src/{}", BINDINGS_FILE));
+    let output_path = PathBuf::from(var("OUT_DIR").expect("env variable OUT_DIR not found"));
+    let path_bindings_buf_src = output_path.join(BINDINGS_FILE);
     let path_bindings_file_src = path_bindings_buf_src.as_os_str().to_str().unwrap();
     let assimp_compiled_lib_path = output_path.join("lib");
     let assimp_compiled_include_path = output_path.join("include");
--- russimp-sys/src/lib.rs
+++ russimp-sys/src/lib.rs
@@ -2,8 +2,8 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 
-mod bindings;
-pub use bindings::*;
+include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
 use std::ffi::CStr;
 
 impl Into<String> for aiString {
